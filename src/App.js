39c5de0edb586338fcc0cc62aa3aea68d3111808
Subhra Mukherjee <subhramukherjee560@gmail.com>
2023-01-08 00:39:57 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -1,16 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
+import "./App.css";
+import { useEffect, useState } from "react";
+import { Images } from "./component/Images";
+import Loader from "./component/Loader";
+import Header from "./component/Header";
+import axios from "axios";
+import { createGlobalStyle } from 'styled-components';
+import InfiniteScroll from 'react-infinite-scroll-component';
+
+import styled from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: sans-serif;
+}
+`;
+
+const WrapperImages = styled.section`
+max-width: 70rem;
+margin: 4rem auto;
+display: grid;
+grid-gap: 1em;
+grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+grid-auto-rows: 300px;
+`;
+
+
 
 function App() {
-  const [value,setValue] = useState()
+  const [images, setImage] = useState([]);
+
+  useEffect(() => {
+    fetchImages();
+  }, []);
+
+
+  const fetchImages = (count=20) => {
+    const apiRoot = "https://api.unsplash.com";
+    // given accesskey inside it just to be secure.
+    const accessKey = process.env.REACT_APP_ACCESSKEY || '6FJ-08sw_QAgTJ84pOrXO6Kx13gGJpXvYfB4SPsvgnQ';
+
+    axios
+      .get(`${apiRoot}/photos/random?client_id=${accessKey}&count=${count}`)
+      .then((res) => {
+        console.log(res.data,'res')
+        setImage([...images, ...res.data]);
+      }); 
+  };
+
+  const getSearchImages  = (searchImages) =>{
+    console.log(searchImages,'search');
+    setImage(searchImages);
+  }
+
   return (
     <div className="App">
-      <div className='mydiv'>
-         <span>Search</span>
-         <input type='text' value={value} onChange={(e)=>setValue(e.target.value)}/>
-         <button>Search</button>
-      </div>
+      <Header getSearchImages={getSearchImages}/>
+      <GlobalStyle />
+      <InfiniteScroll
+        dataLength={images.length}
+        next={fetchImages}
+        hasMore={true}
+        loader={<Loader />}
+      >
+        <WrapperImages>
+          {images.map(image => (
+            <Images url={image.urls.thumb} key={image.id} />
+          ))}
+        </WrapperImages>
+      </InfiniteScroll>
     </div>
   );
 }
---
+++ src/component/Header.js
@@ -0,0 +1,89 @@
+import axios from "axios";
+import React, { useState } from "react";
+import styled from "styled-components";
+import "./header.css";
+
+const Heading = styled.header`
+  max-width: 70rem;
+  margin: 2rem auto;
+  text-align: center;
+`;
+
+const H1 = styled.h1`
+  font-family: "Oswald", sans-serif;
+  margin-bottom: 1em;
+`;
+
+const Input = styled.input`
+  height: 2.5rem;
+  width: 20rem;
+  margin-top: 1em;
+  outline: none;
+  text-indent: 1em;
+  font-size: 1em;
+
+  ::placeholder {
+    font-size: 0.8em;
+  }
+`;
+
+const Button = styled.button`
+  height: 2.5rem;
+  padding: 0 1em;
+  outline: none;
+  cursor: pointer;
+  background: #222;
+  border: none;
+  color: #fff;
+  font-size: 1em;
+`;
+
+const Header = ({ getSearchImages }) => {
+  const [value, setValue] = useState("");
+
+  const SearchForPhotos = (count = 20) => {
+    setValue("");
+    const apiRoot = "https://api.unsplash.com";
+    // given accesskey inside it just to be secure.
+    const accessKey =
+      process.env.REACT_APP_ACCESSKEY ||
+      "mUeTdZoSLI41NtQfPL4L-6HIc35LSZlLdqzfCZVNS94";
+
+    axios
+      .get(
+        `${apiRoot}/search/photos?client_id=${accessKey}&query=${value}&count=${count}`
+      )
+      .then((res) => {
+        getSearchImages(res.data.results);
+      });
+  };
+
+  return (
+    <div className="container">
+      <img
+        src="https://files.oyebesmartest.com/uploads/large/hill-hd-wallpapersrie8e.png"
+        alt="Snow"
+        className="contianer-image"
+      />
+      <div className="centered">
+        <Heading>
+          <H1 style={{fontWeight:'bold'}}>Unsplash Clone</H1>
+          <div>
+            <p style={{fontWeight:'bold'}}>The internet’s source of freely usable images.</p>
+            <p style={{fontWeight:'bold'}}>Powered by creators everywhere.</p>
+          </div>
+
+          <Input
+            type="text"
+            placeholder="Search photos"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+          />
+          <Button onClick={() => SearchForPhotos()} className='search'>Search</Button>
+        </Heading>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
